e89956a123de8047860a759110038e13db7bbd3b
OliverBR1 <Br.0liveira@outlook.com>
2025-07-15 22:47:23 -0300

    Updated project

--- hello.go
+++ hello.go
@@ -1,83 +1,145 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"time"
+    "bufio"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "strconv"
+    "strings"
+    "time"
 )
 
 const monitoramentos = 2
 const delay = 5
 
 func main() {
-	exibeIntroducao()
-	for {
-		exibeMenu()
+    exibeIntroducao()
+    for {
+        exibeMenu()
+        comando := leComando()
 
-		comando := leComando()
-
-		switch comando {
-		case 1:
-			iniciarMonitoramento()
-		case 2:
-			fmt.Println("Exibindo Logs...")
-		case 0:
-			fmt.Println("Saindo do programa")
-			os.Exit(0)
-		default:
-			fmt.Println("Não conheço este comando")
-			os.Exit(-1)
-		}
-	}
-
+        switch comando {
+        case 1:
+            iniciarMonitoramento()
+        case 2:
+            //Chamando aqui
+            fmt.Println("Exibindo Logs...")
+            imprimeLogs()
+        case 0:
+            fmt.Println("Saindo do programa")
+            os.Exit(0)
+        default:
+            fmt.Println("Não conheço este comando")
+            os.Exit(-1)
+        }
+    }
 }
 
 func exibeIntroducao() {
-	nome := "Douglas"
-	versao := 1.2
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
+    nome := "Bruno"
+    versao := 1.2
+    fmt.Println("Olá, sr.", nome)
+    fmt.Println("Este programa está na versão", versao)
 }
 
 func exibeMenu() {
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
+    fmt.Println("1- Iniciar Monitoramento")
+    fmt.Println("2- Exibir Logs")
+    fmt.Println("0- Sair do Programa")
 }
 
 func leComando() int {
-	var comandoLido int
-	fmt.Scan(&comandoLido)
-	fmt.Println("O comando escolhido foi", comandoLido)
-	fmt.Println("")
+    var comandoLido int
+    fmt.Scan(&comandoLido)
+    fmt.Println("O comando escolhido foi", comandoLido)
+    fmt.Println("")
 
-	return comandoLido
+    return comandoLido
 }
 
 func iniciarMonitoramento() {
-	fmt.Println("Monitorando...")
+    fmt.Println("Monitorando...")
 
-	sites := []string{"https://random-status-code.herokuapp.com/", "https://www.alura.com.br", "https://www.caelum.com.br"}
+    sites := leSitesDoArquivo()
 
-	for i := 0; i < monitoramentos; i++ {
-		for i, site := range sites {
-			fmt.Println("Testando site", i, ":", site)
-			testaSite(site)
-		}
-		time.Sleep(delay * time.Second)
-		fmt.Println("")
-	}
+    for i := 0; i < monitoramentos; i++ {
+        for i, site := range sites {
+            fmt.Println("Testando site", i, ":", site)
+            testaSite(site)
+        }
+        time.Sleep(delay * time.Second)
+        fmt.Println("")
+    }
 
-	fmt.Println("")
+    fmt.Println("")
 }
 
 func testaSite(site string) {
-	resp, _ := http.Get(site)
+    resp, err := http.Get(site)
 
-	if resp.StatusCode == 200 {
-		fmt.Println("Site:", site, "foi carregado com sucesso!")
-	} else {
-		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
-	}
+    if err != nil {
+        fmt.Println("Ocorreu um erro:", err)
+    }
+
+    if resp.StatusCode == 200 {
+        fmt.Println("Site:", site, "foi carregado com sucesso!")
+        registraLog(site, true)
+    } else {
+        fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+        registraLog(site, false)
+    }
+}
+
+func leSitesDoArquivo() []string {
+
+    var sites []string
+
+    arquivo, err := os.Open("sites.txt")
+
+    if err != nil {
+        fmt.Println("Ocorreu um erro:", err)
+    }
+
+    leitor := bufio.NewReader(arquivo)
+    for {
+        linha, err := leitor.ReadString('\n')
+        linha = strings.TrimSpace(linha)
+
+        sites = append(sites, linha)
+
+        if err == io.EOF {
+            break
+        }
+
+    }
+
+    arquivo.Close()
+
+    return sites
+}
+
+func imprimeLogs() {
+
+    arquivo, err := ioutil.ReadFile("log.txt")
+
+    if err != nil {
+        fmt.Println("Ocorreu um erro:", err)
+    }
+
+    fmt.Println(string(arquivo))
+}
+
+func registraLog(site string, status bool) {
+
+    arquivo, err := os.OpenFile("log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+    if err != nil {
+        fmt.Println("Ocorreu um erro:", err)
+    }
+    arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+    arquivo.Close()
 }
